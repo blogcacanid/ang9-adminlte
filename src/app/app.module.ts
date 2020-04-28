ff6252ad3184c99f52afa8f3d4a4b3ec4d563467
rokib <akun.rokib.id@gmail.com>
2020-04-28 19:09:24 +0700

    publish file index

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppheaderComponent } from './appheader/appheader.component';
+import { AppmenuComponent } from './appmenu/appmenu.component';
+import { AppfooterComponent } from './appfooter/appfooter.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    DashboardComponent,
+    Page1Component,
+    Page2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/appheader/appheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appheader',
+  templateUrl: './appheader.component.html',
+  styleUrls: ['./appheader.component.scss']
+})
+export class AppheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
